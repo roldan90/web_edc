e90b45498c8c6618a38868378a69265b38d3c096
Roldan Aquino Segura <roldanaquinosegura@gmail.com>
2023-11-04 01:42:12 -0600

    Alebrijes

---
+++ assets-alebrije/js/app.js
@@ -0,0 +1,9 @@
+function mostrarModal(id) {
+    let modal = document.getElementById(id);
+    modal.showModal();
+}
+
+function cerrarModal(id) {
+    let modal = document.getElementById(id);
+    modal.close();
+}
